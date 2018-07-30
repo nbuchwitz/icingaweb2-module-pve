60817133b7e075a678c105dccc84897f623c1c67
Nicolai Buchwitz <nicolai.buchwitz@enda.eu>
2018-07-30 15:10:23 +0200

    Add subscription details to Nodes

--- library/Pve/ManagedObject/HostSystem.php
+++ library/Pve/ManagedObject/HostSystem.php
@@ -6,7 +6,6 @@ use Icinga\Module\Pve\Api;
 
 class HostSystem extends ManagedObject
 {
-
     public static function getColumns()
     {
         return array(
@@ -19,8 +18,23 @@ class HostSystem extends ManagedObject
             "memory_size",
             "pve_version",
             "kernel_version",
-            "storages"
+            "storages",
+            "subscription_productname",
+            "subscription_status",
+            "subscription_sockets",
+            "subscription_duedate",
         );
+    }
+
+    private static function fetchSubscriptionDetails(Api $api, $node) {
+        $subscription = $api->get(sprintf("/nodes/%s/subscription", $node));
+
+        return [
+            "subscription_productname" => $subscription['productname'],
+            "subscription_status" => $subscription['status'],
+            "subscription_sockets" => $subscription['sockets'],
+            "subscription_duedate" => $subscription['nextduedate'],
+        ];
     }
 
     public static function fetch(Api $api, $fetchStorages = true)
@@ -33,14 +47,16 @@ class HostSystem extends ManagedObject
             $node = [
                 "name" => $el['node'],
                 "cpu_model" => $nodeStatus['cpuinfo']['model'],
-                "cpu_speed" => $nodeStatus['cpuinfo']['mhz'],
-                "cpu_cores" => $nodeStatus['cpuinfo']['cpus'],
+                "cpu_speed" => (int)$nodeStatus['cpuinfo']['mhz'],
+                "cpu_cores" => (int)$nodeStatus['cpuinfo']['cpus'],
                 "cpu_sockets" => $nodeStatus['cpuinfo']['sockets'],
                 "cpu_hvm" => $nodeStatus['cpuinfo']['hvm'] == 1,
                 "memory_size" => (int)$nodeStatus['memory']['total'] / 1024 / 1024 / 1024,
                 "pve_version" => $nodeStatus['pveversion'],
                 "kernel_version" => $nodeStatus['kversion'],
             ];
+
+            $node = array_merge($node, HostSystem::fetchSubscriptionDetails($api, $el['node']));
 
             if ($fetchStorages) {
                 $node['storages'] = Storage::fetchStorageDetails($api, $el['node']);
